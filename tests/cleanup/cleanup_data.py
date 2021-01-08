9ea6ea58e03a7417ea2468b2b428867f4ed531f6
widal001 <williamdaly422@gmail.com>
2021-01-07 19:03:46 -0500

    Creates test folder for cleanup with an initial data file

---
+++ tests/cleanup/cleanup_data.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+president = {
+    "year": [1976, 1976, 1976, 1976, 1976],
+    "state": ["Alabama", "Alabama", "Alabama", "Alabama", "Alabama"],
+    "state_po": ["AL", "AL", "AL", "AL", "AL"],
+    "state_fips": [1, 1, 1, 1, 1],
+    "state_cen": [63, 63, 63, 63, 63],
+    "state_ic": [41, 41, 41, 41, 41],
+    "office": [
+        "US President",
+        "US President",
+        "US President",
+        "US President",
+        "US President",
+    ],
+    "candidate": [
+        "Carter, Jimmy",
+        "Ford, Gerald",
+        "Maddox, Lester",
+        'Bubar, Benjamin ""Ben""',
+        "Hall, Gus",
+    ],
+    "party": [
+        "democrat",
+        "republican",
+        "american independent party",
+        "prohibition",
+        "communist party use",
+    ],
+    "writein": [False, False, False, False, False],
+    "candidatevotes": [659170, 504070, 9198, 6669, 1954],
+    "totalvotes": [1182850, 1182850, 1182850, 1182850, 1182850],
+    "version": [20171015, 20171015, 20171015, 20171015, 20171015],
+    "notes": [np.nan, np.nan, np.nan, np.nan, np.nan],
+}
